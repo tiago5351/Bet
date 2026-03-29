181c82216e4494811a94ba707996bad7c40efdfb
tiago5351 <tiagoblatter@gmail.com>
2026-03-29 18:00:04 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -63,3 +63,15 @@ self.addEventListener('notificationclick', e => {
     })
   );
 });
+
+ self.addEventListener('push', event => {
+  const data = event.data ? event.data.json() : {};
+
+  event.waitUntil(
+    self.registration.showNotification(data.title || 'BetTrack', {
+      body: data.body || '',
+      icon: '/icon-192.png',
+      badge: '/icon-192.png'
+    })
+  );
+});
