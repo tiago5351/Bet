b504a93b554ad34f9f46a64b3bd8a047e590b685
tiago5351 <tiagoblatter@gmail.com>
2026-03-30 07:41:05 -0300

    Update send-push.js

--- api/send-push.js
+++ api/send-push.js
@@ -13,6 +13,13 @@ webpush.setVapidDetails(
 );
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+if (req.method === 'OPTIONS') {
+  return res.status(200).end();
+}
   try {
     const { data, error } = await sb
       .from('push_subscriptions')
